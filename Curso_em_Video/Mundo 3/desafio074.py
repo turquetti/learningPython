c05a997c792943affeb845777a5c5161ae70d73d
turquetti <gabrielaturquetti@gmail.com>
2021-03-01 13:51:27 -0300

    adding new files

--- Curso_em_Video/Mundo 3/desafio074.py
+++ Curso_em_Video/Mundo 3/desafio074.py
@@ -1,2 +1,9 @@
 # um programa que ira gerar 5 numeros aleatorios e colocar em uma tupla
 # mostre a listagem dos numeros gerados e indique o menor e o maior.
+
+import random
+
+random_list = random.randint(0, 100)
+
+for i in random_list:
+    
