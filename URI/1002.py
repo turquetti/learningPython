c359d81b510961f103ff113cac3c0aa8f5132121
turquetti <gabrielaturquetti@gmail.com>
2021-03-11 21:23:29 -0300

    adding URI

---
+++ URI/1002.py
@@ -0,0 +1,5 @@
+R = float(input())
+n = 3.14159
+A = n * (R**2)
+
+print(f"A={A:.4f}")
---
+++ URI/1008.py
@@ -0,0 +1,8 @@
+enumber = int(input())
+hours = int(input())
+payperhour = float(input())
+
+salary = float(hours * payperhour)
+
+print(f"NUMBER = {enumber}")
+print(f"SALARY = U$ {salary:.2f}")
